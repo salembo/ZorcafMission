5e0e2359100a59de12b9822cb92dc27dbc49fa0f
salembo <ana.m.r.ruiz@gmail.com>
2014-04-12 15:45:57 -0400

    First Commit
    First Commit of ZorcafMission

---
+++ ZorcafMission/main.lua
@@ -0,0 +1,14 @@
+-----------------------------------------------------------------------------------------
+--
+-- main.lua
+--
+-----------------------------------------------------------------------------------------
+
+--display.newText("Hello World!",0,20,native.systemFont,16);
+local photo=display.newImage("images/hubble.jpg",0,40);
+
+local rect1=display.newRect(10,35,150,50);
+rect1:setFillColor(51,255,0);
+rect1.strokeWidth=8;
+rect1:setStrokeColor(80,200,130);
+
